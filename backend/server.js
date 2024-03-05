48befaab2abfbfb3d14f84c09e08e4ccab80b7f4
Chowdhury Md. Rabby Mahmud <md.rabby.mahmud@gmail.com>
2024-03-05 12:15:12 +0600

    connect mongo database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 const app = require("./app");
-
 const dotenv = require("dotenv");
+const connectDatabase = require("./config/database");
 
 //Config
-
 dotenv.config({path:"backend/config/config.env"});
 
+//After config connecting to database
+connectDatabase();
 
 
 app.listen(process.env.PORT, () => {
